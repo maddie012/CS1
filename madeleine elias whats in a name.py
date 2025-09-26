d3ac3fa150f3bcc4a80b58bb356cfd32318da36d
maddie012 <melias28@gcds.net>
2025-09-26 09:56:11 -0400

    Add files via upload

--- madeleine elias whats in a name.py
+++ madeleine elias whats in a name.py
@@ -9,18 +9,41 @@
 import random
 def count_vowels(name):
     '''
-    counts the vowels in the name
+    counts the vowels in the name and knows the totals of each vowel
     Args:
         name: the name that the user inputed 
     returns:
-        the number of vowels in the name'''
+        the number of vowels in the name and the number of each vowel in the name'''
+    a = 0
+    e = 0
+    ilet = 0
+    o = 0
+    u = 0
+    y = 0
     count = 0
     for i in name:
-        if i == "a" or i == "e" or i =="i" or i == "o" or i == "u"or i=="y" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U" or i=="Y":
-            count = count + 1
+        if i == "a" or i =="A":
+            a = a + 1
+            count = count +1
+        elif i == "e" or i=="E":
+            e = e +1
+            count = count +1
+        elif i == "i" or i=="I":
+            ilet = ilet+1
+            count = count+1
+        elif i=="o" or i=="O":
+            o=o+1
+            count = count +1
+        elif i=="u" or i=="U":
+            u=u+1
+            count + count +1
+        elif i=="y" or i=="Y":
+            y=y+1
+            count = count +1
         else:
             count = count + 0
-    return count
+    message = f"A:{a} E:{e} I:{ilet} O:{o} U:{u} Y:{y} total:{count}"
+    return message
 
 def reverse_display(name):
     '''
@@ -33,16 +56,17 @@ def reverse_display(name):
 
 def palindrome(name1):
     '''
-    check if the name is a palindrome and prints true or false depending on if it is
+    check if the name is a palindrome and prints true or false depending on if it is uses function lower to turn the first name to lowercase
     Args:
         name1: the first name of the user after the  full name got put into the function first_name()
     returns:
         true or false depending on the answer'''
-    revname = name1[::-1]
-    print(name1 == revname)
+    name2 = lower(name1)
+    revname = name2[::-1]
+    print(name2 == revname)
 
 def count_consonant(name):
-    '''counts the number of consonants 
+    '''counts the number of consonants by using for loops subtracts every time there is a space because it should not count for total letters
     Args:
         name: the name the user inputed
     returns:
@@ -54,9 +78,10 @@ def count_consonant(name):
     for i in name:
         if i == "q" or i =="w" or i =="r" or i=="t" or i=="p"or i=="s"or i=="d"or i=="f"or i=="g"or i=="h"or i=="j"or i=="k"or i=="l"or i=="z" or i=="x" or i=="c" or i=="v" or i=="b" or i=="n" or i=="m" or i=="Q" or i=="W" or i=="R" or i=="T" or i=="P" or i=="S" or i=="D" or i=="F" or i=="G" or i=="H" or i=="J" or i=="K" or i=="L" or i=="Z" or i=="X" or i=="C" or i=="V" or i=="B" or i=="N" or i=="M":
             count = count +1
+        if i ==" ":
+            total = total -1
         else:
             count = count + 0
-    total = total - 2
     frequency = count/total
     return frequency
 
@@ -130,7 +155,6 @@ def hyphen(name3):
             random = random +0
     return False
             
-
 def random_name():
     '''
     function chooses a random length between 5-15 for the length of the name. then it joins random letters for that length to get a random name
@@ -154,7 +178,9 @@ def initials(name1,name2,name3):
     middle = list(name2)
     last = list(name3)
     all = (first[0],middle[0],last[0])
-    return all
+    sep = ","
+    initials = sep.join(all)
+    return initials
 
 def lower(name):
     '''
@@ -214,16 +240,26 @@ def mix_name(namef):
     mix = ""
     name1 = list(namef)
     while len(name1) > 0:
-        find = random.choice(name1)
+        namnum = len(name1) - 1
+        number = random.randint(0,namnum)
+        find = name1[number]
         mix += find
         name1.remove(find)
     return mix
 
 def full_scramble(name):
+    '''
+    function uses the full name and scrambles it by turning it into a list and picking random indexs then deleting them to make original name smaller
+    Args:
+        name: the full name the user imported
+    returns:
+        a scramble of the users full name'''
     scramble = ""
     namel = list(name)
     while len(namel) > 0:
-        find = random.choice(namel)
+        namnum = len(namel) -1
+        number = random.randint(0,namnum)
+        find = namel[number]
         scramble += find
         namel.remove(find)
     return scramble
@@ -231,13 +267,13 @@ def full_scramble(name):
 
 
 def main():
-    name = input("enter your fullname ")
+    name = input("enter your fullname (you need a middle name for all functions) ")
+    print("what do you want to do?")
     name1 = first_name(name)
     name2 = middle_name(name)
     name3 = last_name(name)
     while True:
-        print("what do you want to do?")
-        question = input("vowels/reverse/consonants/random/first name/middle name/last name/uppercase/lowercase/initials/palindrome/hyphen/title/mix/scramble ")
+        question = input("vowels/reverse/consonants/random/first name/middle name/last name/uppercase/lowercase/initials/palindrome/hyphen/title/mix/scramble/exit ")
 
         if question =="vowels":
             print("number of vowels:")
@@ -284,11 +320,10 @@ def main():
         elif question == "scramble":
             print("scramble of full name")
             print(full_scramble(name))
+        elif question == "exit":
+            exit()
         else:
             print("choose an option ")
     
 main()
      
-
-   
-
